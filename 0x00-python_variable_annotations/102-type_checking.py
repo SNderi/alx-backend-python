b96754edb978bf65a2a1f48c36b61f427b3a9f2d
Sharon Nderi <nderisharonw@gmail.com>
2023-03-21 20:07:39 +0300

    Edit advanced task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Module to validate a piece of code using mypy. """
 
-from typing import List, Tuple, Union, Any
+from typing import List, Tuple
 
 
-def zoom_array(lst: List, factor: Any = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """ Returns a list zoomed in by a factor. """
     zoomed_in: List = [
         item for item in lst
@@ -17,4 +17,4 @@ array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
